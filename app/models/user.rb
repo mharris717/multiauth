f2c03d577692f904b450f3ff2cf8a13f2bd2ef7c
Mike Harris <mharris717@gmail.com>
2013-06-10 09:10:53 -0400

    Moved user logic to module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,38 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
-         
-  has_many :identities
-  
-  validates_presence_of :email
-
-
-  # Setup accessible (or protected) attributes for your model
-  # attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
-  
-  def has_identity?(provider)
-    identities.where(:provider => provider.to_s).count > 0
-  end
-  
-  def self.find_for_putio_oauth(auth, signed_in_resource=nil)
-    UserFind.get(auth, :current_user => signed_in_resource)
-  end
-  
-  def self.find_for_dropbox_oauth(auth, signed_in_resource=nil)
-    UserFind.get(auth, :current_user => signed_in_resource)
-  end
-  
-  def self.find_for_evernote_oauth(auth, signed_in_resource=nil)
-    UserFind.get(auth, :current_user => signed_in_resource)
-  end
-
-  def self.find_for_google_oauth2(auth, signed_in_resource=nil)
-    UserFind.get(auth, :current_user => signed_in_resource)
-  end
-
-  def get_ident(provider)
-    identities.select { |x| x.provider == provider.to_s }.first
-  end
+  puts "including usermod in multiauth"
+  include UserMod
 end
 
---
+++ app/models/user_mod.rb
@@ -0,0 +1,44 @@
+module UserMod
+  extend ActiveSupport::Concern
+
+  included do
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+           
+    has_many :identities
+    
+    validates_presence_of :email
+  end
+
+  def has_identity?(provider)
+    identities.where(:provider => provider.to_s).count > 0
+  end
+
+  module ClassMethods
+    def find_for_putio_oauth(auth, signed_in_resource=nil)
+      UserFind.get(auth, :current_user => signed_in_resource)
+    end
+    
+    def find_for_dropbox_oauth(auth, signed_in_resource=nil)
+      UserFind.get(auth, :current_user => signed_in_resource)
+    end
+    
+    def find_for_evernote_oauth(auth, signed_in_resource=nil)
+      UserFind.get(auth, :current_user => signed_in_resource)
+    end
+
+    def find_for_google_oauth2(auth, signed_in_resource=nil)
+      UserFind.get(auth, :current_user => signed_in_resource)
+    end
+
+    puts 'find for fatsecret method'
+
+    def find_for_fatsecret(auth, signed_in_resource=nil)
+      UserFind.get(auth, :current_user => signed_in_resource)
+    end
+  end
+
+  def get_ident(provider)
+    identities.select { |x| x.provider == provider.to_s }.first
+  end
+end
